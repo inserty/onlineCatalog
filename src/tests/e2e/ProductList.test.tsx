a9b0ab571252ab52e74447e3fa3938ddf7af0ff1
Pawel Krol <pawel.krol@axxiome.com>
2021-08-06 14:31:16 +0200

    Add cypress. Provide some tests

--- src/tests/e2e/ProductList.test.tsx
+++ src/tests/e2e/ProductList.test.tsx
@@ -7,16 +7,19 @@ import configureMockStore from "redux-mock-store";
 const mockStore = configureMockStore();
 const store = mockStore({});
 
-it('renders product list catalog component', () => {
-  mount(<Provider store={store}><ProductsList /></Provider>);
-});
 
-it('renders no product information', () => {
-  mount(<Provider store={store}><ProductsList /></Provider>);
-  cy.visit('http://localhost:3001')
-  cy.window().should('have.property', '__store__');
-  // cy.window().its('__store__')
-  //   .then(
-  //     store => store.dispatch({ type: 'UPDATE_CURRENT_PROFILE' })
-  //   );
-});
+context('Product list', () => {
+
+    before(() => {
+      cy.visit('http://localhost:3001')
+    })
+
+    it('renders product list catalog component', () => {
+      mount(<Provider store={store}><ProductsList /></Provider>);
+    });
+
+    it('renders no product information', () => {
+
+    });
+
+})
